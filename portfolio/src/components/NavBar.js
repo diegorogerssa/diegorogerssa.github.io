d3c48d7fa867d79047a158a674308469de1b7da5
Diego Rogers <rogersmhdg@gmail.com>
2022-07-23 23:15:21 -0300

    crição portfólio em andamento 0.3 menu responsivo

--- portfolio/src/components/NavBar.js
+++ portfolio/src/components/NavBar.js
@@ -3,16 +3,39 @@ import '../styles/NavBar.css'
 import { NavLink } from "react-router-dom";
 
 class NavBar extends Component{
+  constructor(props){
+    super()
+      this.state = {
+        menu: 'nav-close',
+      }
+  }
+mudarClasseOpen = () => {
+  const { menu } = this.state
+    menu === 'nav-close'? this.setState({ menu:'nav-container' }):this.setState({ menu:'nav-close' })
+}
+
+mudarClasseClose = () => {
+  const { menu } = this.state
+    menu === 'nav-close'? this.setState({ menu:'nav-container' }):this.setState({ menu:'nav-close' })
+}
+
   render(){
+    const { menu } = this.state
     return(
-      <nav className = 'nav-container'>
-        <h1>&lt; Diego Rogers /&gt;</h1>
-        <ul>
-          <li><NavLink className='nav-bar-link' activeClassName="selected" to='/'>&lt; Home /&gt;</NavLink></li>
-          <li><NavLink className='nav-bar-link' activeClassName="selected" to='/projetos'>&lt; Projetos /&gt;</NavLink></li>
-          <li><NavLink className='nav-bar-link' activeClassName="selected" to='/sobre'>&lt; Sobre mim /&gt;</NavLink></li>
-        </ul>
+      <header className='header-container'>
+        
+          <h1>&lt; Diego Rogers /&gt;</h1>
+          <a onClick={this.mudarClasseOpen} className='icon-menu' href="#nav-container">&#x0224B;</a>
+        
+        <nav id='nav-container' className = { menu }>
+          <ul>
+            <li><NavLink className='nav-bar-link' activeClassName="selected" to='/'>&lt; Home /&gt;</NavLink></li>
+            <li><NavLink className='nav-bar-link' activeClassName="selected" to='/projetos'>&lt; Projetos /&gt;</NavLink></li>
+            <li><NavLink className='nav-bar-link' activeClassName="selected" to='/sobre'>&lt; Sobre mim /&gt;</NavLink></li>
+            <a onClick={this.mudarClasseClose} className='icon-close' href="#nav-container">X</a>
+          </ul>
       </nav>
+      </header>
     )
   }
 }
