8b69df55de8f96c6ee23998a41c3c2818733ff2f
Diego Rogers <rogersmhdg@gmail.com>
2022-07-25 21:36:24 -0300

    correção link pokedex

--- portfolio/src/pages/Projetos.js
+++ portfolio/src/pages/Projetos.js
@@ -13,17 +13,17 @@ class Projetos extends Component{
             <a href='https://diegorogerssa.github.io/ProjetosDiversos/'>
               <CardProjetos
                 title = 'Pixel Art'
-                tools = 'ferramentas usadas'
+                tools = 'HTML, CSS, JAVA SCRIPT'
                 image = 'https://i.pinimg.com/originals/a9/c8/9e/a9c89e126fe0b97bd3369e3de726decd.png'
-                description = 'Descrição do projeto'
+                description = 'Projeto pessoal para fins acadêmicos, um quadro simples de pintura em pixel art feito puramente com HTML, CSS e Java Script. '
               />
             </a>
-            <a href="https://diegorogerssa.github.io/pokedex-react/">
+            <a href="https://projeto-pokedex-api.herokuapp.com/">
               <CardProjetos
-                title = 'PokedexX'
-                tools = 'ferramentas usadas'
+                title = 'API Pokedex'
+                tools = 'REACT JS'
                 image = 'https://www.pngplay.com/wp-content/uploads/11/Heliolisk-Pokemon-PNG-HD-Quality.png'
-                description = 'Descrição do projeto'
+                description = 'Projeto pessoal para fins acadêmicos, no projeto a requisições a API do Pokedex e a manipulação das informações recebidas'
               />
             </a>
         </div>
